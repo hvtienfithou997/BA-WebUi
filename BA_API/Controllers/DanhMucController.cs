997af0e365c9259aa0d37ae215acc9dd61735183
Viet Tien <hvtien.fithou@gmail.com>
2021-05-26 11:37:38 +0700

    #1 commit code

---
+++ BA_API/Controllers/DanhMucController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BA_API.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        [Route("danh-muc/phuong-tien")]
+        public IActionResult PhuongTien(string providerId, string departmentId, string companyId, string vehicle, string type, int index = 0)
+        {
+            ViewBag.providerId = providerId;
+            ViewBag.departmentId = departmentId;
+            ViewBag.companyId = companyId;
+            ViewBag.vehicle = vehicle;
+            ViewBag.type = type;
+            ViewBag.index = index;
+            return View();
+        }
+
+        [Route("danh-muc/don-vi-van-tai")]
+        public IActionResult VanTai(string departmentid, string companyid, int index = 0)
+        {
+            ViewBag.departmentid = departmentid;
+            ViewBag.companyid = companyid;
+            ViewBag.index = index;
+            return View();
+        }
+
+        [Route("danh-muc/danh-sach-tinh")]
+        public IActionResult DanhSachTinh()
+        {
+            return View();
+        }
+        [Route("danh-muc/danh-sach-don-vi-truyen-du-lieu")]
+        public IActionResult DonViTruyenDuLieu()
+        {
+            return View();
+        }
+    }
+}
